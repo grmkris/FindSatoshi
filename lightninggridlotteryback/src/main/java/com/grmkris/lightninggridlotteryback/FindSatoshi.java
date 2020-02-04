e0b0a99ce4b92e1547027b45c50895955eea0906
grmkris <kristjan.grm1@gmail.com>
2020-02-04 20:13:41 +0100

    feat(FindSatoshi.java): Application need active round to so it can issue tickets, on startup we start round. if round is already running exception is thrown

--- lightninggridlotteryback/src/main/java/com/grmkris/lightninggridlotteryback/FindSatoshi.java
+++ lightninggridlotteryback/src/main/java/com/grmkris/lightninggridlotteryback/FindSatoshi.java
@@ -5,8 +5,10 @@ import com.grmkris.lightninggridlotteryback.model.database.Round.Round;
 import com.grmkris.lightninggridlotteryback.service.RoundService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -14,20 +16,19 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class FindSatoshi {
 
-	@Autowired
-	static RoundService roundService;
-
 	public static void main(String[] args) {
 		SpringApplication.run(FindSatoshi.class, args);
-		try {
-			Round round = startGame();
-			log.info("Started new ROUND: " + round.getStartDate().getTime());
-		} catch (RoundRunningException e) {
-			log.info("Round already running");
-		}
-	}
 
-	public static Round startGame() throws RoundRunningException {
-		return roundService.newRound();
+	}
+	@Bean
+	public CommandLineRunner init(RoundService roundService) {
+		return (args) -> {
+			try {
+				Round round = roundService.newRound();
+				log.info("Started new ROUND: " + round.getStartDate().getTime());
+			} catch (RoundRunningException e) {
+				log.info("Round already running");
+			}
+		};
 	}
 }
